4116d3385dff5c814d60e7079039e6aa8abbb1f1
Connor Tan <connor.tan@gmail.com>
2023-05-22 23:24:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My first personal Github Project
+This is my first personal GitHub project. Published directly from visual studio code.
---
+++ index.js
@@ -0,0 +1,8 @@
+//document.getElementById("count").innerText = 7
+
+//creating two variables
+let count = 5
+
+count = 3
+
+console.log(count)
